6251a15762988fecb7272c040c9765d3025d1d27
GatienVilain <gatien.vilain@outlook.fr>
2023-05-26 20:07:58 +0200

    improve main file

--- main.py
+++ main.py
@@ -3,23 +3,23 @@ This file is the main file for the project. It will run all the other files
 """
 import os
 from trading_simulator import app
+from trading_simulator.Setup import download_market_data, scraping_news
 
-# Import data if not already done
-if not os.path.exists("Data"):
-    from trading_simulator.Setup import download_market_data, scraping_news
-
-    # Run all setup files
-    print('##### Setup #####'\
-        '\nDownloading market data...\n')
-    download_market_data()
-
-    print('\nScrapping news...\n')
-    scraping_news()
-
-    print('\nSetup done')
-
-# Run the app
 if __name__ == '__main__':
-    print('\n##### Running app #####\n')
-    app.run_server(debug=True) #TODO: change to False when deploying
+    # Run all setup files if not already done
+
+    if not os.path.exists('Data'):
+        print('Creating directory Data at root of the project')
+        os.mkdir('Data')
+
+    if not os.path.exists(os.path.join('Data', "market_data.csv")):
+        print('\nDownloading market data...\n')
+        download_market_data()
+
+    if not os.path.exists(os.path.join('Data', "news.csv")):
+        print('\nScrapping news...\n')
+        scraping_news()
+        print('Done\n')
+
+app.run_server(debug=True) #TODO: change to False when deploying
 
