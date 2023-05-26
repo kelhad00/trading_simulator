5599f138818bddff266e9b36cc44e052e8e22eee
GatienVilain <gatien.vilain@outlook.fr>
2023-05-26 20:02:29 +0200

    Add comment on setup function

--- trading_simulator/Setup/__init__.py
+++ trading_simulator/Setup/__init__.py
@@ -5,7 +5,11 @@ Provides scripts files as functions
 """
 
 def download_market_data():
+    """ Download market data from Yahoo Finance and save it a Data folder
+    """
     importlib.import_module('.download_market_data', __package__)
 
 def scraping_news():
+    """ Scrap news from ZoneBourse and save it a Data folder
+    """
     importlib.import_module('.scraping_with_bs4', __package__)
