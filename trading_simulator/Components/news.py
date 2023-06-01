bd97e9b0e4ff8577f09ef9412464ab899d96e329
Capoupacap <capucine.fouillard@student.junia.com>
2023-06-01 15:12:55 +0200

    news layout update

--- trading_simulator/Components/news.py
+++ trading_simulator/Components/news.py
@@ -35,3 +35,43 @@ def update_news_table(timestamp, news_df, idx, range=10):
 	nl = news_df.iloc[idx - range : idx].iloc[::-1]
 
 	return idx, news_df.to_dict(), nl.to_dict('records')
+
+
+
+@app.callback(
+	Output(component_id = 'description-container', component_property = 'style'),
+	Output(component_id = 'news-container', component_property = 'style'),
+	Output(component_id = 'description-text', component_property = 'children'),
+	Input(component_id = 'news-table', component_property = 'active_cell'),
+	State(component_id = 'news-dataframe', component_property = 'data'),
+	State(component_id = 'news-index', component_property = 'data')
+	)
+def show_hide_element(cell_clicked, news_df, idx):
+	"""Hide News table & Show News description when News table cell clicked
+	"""
+	# get the dataFrame for the summary and title
+	# Setup > news_scrapping
+
+	# get the index of the cell clicked
+	print(cell_clicked)
+
+	text_description = 'Résumé de l\'article'
+
+	# change the layout
+	if not cell_clicked :
+		return {'display': 'none'}, {'display': 'block', 'padding': 10, 'flex': 1}, text_description
+		
+	if cell_clicked :
+		return {'display': 'block', 'padding': 10, 'flex': 1}, {'display': 'none'}, text_description
+
+
+# Button to go back to the Market News List
+@app.callback(
+	Output(component_id = 'description-container', component_property = 'style', allow_duplicate=True),
+	Output(component_id = 'news-container', component_property = 'style',allow_duplicate=True),
+	Input(component_id = 'back-to-news-list', component_property = 'n_clicks'),
+	prevent_initial_call=True,
+)
+def back_to_news(btn):
+	return {'display': 'none'}, {'display': 'block', 'padding': 10, 'flex': 1}
+
--- trading_simulator/Setup/news_scrapping.py
+++ trading_simulator/Setup/news_scrapping.py
@@ -14,31 +14,29 @@ COMP = [ # List of stocks to download
 # df = sn.summarize()
 
 # READ THE DATA
-dff = pd.read_csv('data/news.csv',sep=';',usecols=['stock','title','p_date']) # 'sentiment_title'
+dff = pd.read_csv('data/news.csv',sep=';',usecols=['stock','title','summary','p_date']) # 'sentiment_title'
 
 
-# get current date
-current_date = datetime.date.today()
-date = datetime.date(2023, 5, 19)
-# print(date)
+def title_wanted():
+    list_tile = list(dff['title'])
+    
+    # print(list_title)
+    for T in list_title:
+        if 'The title' in T: 
+            print(dff[dff['summary'][dff['title']==T]]) #n possible ?
+            #Add to the text descrption
+            text_desc = str(dff[dff['summary'][dff['title']==T]])
 
-
-def date_wanted():
-    liste_date = list(dff['p_date'])
-    wanted_dates_ind = []
-    wanted_dates=[]
-    # print(liste_date)
-    for d in liste_date:
-        if str(date) in d: 
-            print(d)
-            wanted_dates_ind.append(liste_date.index(d))
-            # wanted_dates.append(dff[dff['p_date']==d])
-            # print(dff[dff['p_date']==d])
-    print(wanted_dates_ind)
-    return wanted_dates
+    return text_desc
 
 date_wanted()
 
 # my_df = dff[dff['p_date']=='CAP.PA_2023-05-02']
 # required_df =df.loc[df['hits']>20]
 # print(my_df)
+
+
+# get current date
+# current_date = datetime.date.today()
+# date = datetime.date(2023, 5, 19)
+# print(date)
